073cff8b47cde6e7f9bfbc482945ba94f60d55de
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-03-25 19:55:41 +0200

    Add generate command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide a command");
+        std::process::exit(1);
+    }
+
+    let command = &args[1];
+
+    match &command[..] {
+        "generate" => generate_keys(),
+        _ => println!("Unknown command {command}"),
+    };
+}
+
+fn generate_keys() {
+    println!("Generating keys");
 }
