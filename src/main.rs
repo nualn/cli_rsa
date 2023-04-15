6a361195520b15e8dc65a7dbb35ef8f88748ea21
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-04-15 12:02:40 +0300

    Refactor keys

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use num_bigint::{BigInt, RandomBits};
-use rand::Rng;
-use rsa::algorithms::{self, extended_eucledian};
-
-use std::fs::File;
-use std::io::prelude::*;
-
-const KEY_SIZE: u64 = 1024;
-const MR_ITERATIONS: isize = 4;
-const DEFAULT_E: i32 = 65_537;
+use rsa::keys::KeyPair;
 
 fn main() {
     // TODO: Parse args with clap
@@ -30,77 +21,4 @@ fn main() {
         }
         _ => println!("Unknown command {command}"),
     };
-}
-
-struct Key {
-    exp: BigInt,
-    modulus: BigInt,
-}
-
-impl Key {
-    fn write_to_file(&self, path: &str) -> std::io::Result<()> {
-        let key_string = self.modulus.to_string() + "\n" + &self.exp.to_string();
-
-        let mut file = File::create(path)?;
-        file.write_all(&key_string.as_bytes())?;
-        Ok(())
-    }
-}
-struct KeyPair {
-    public: Key,
-    private: Key,
-}
-
-impl KeyPair {
-    fn generate() -> KeyPair {
-        loop {
-            let p = generate_probable_prime();
-            let q = generate_probable_prime();
-
-            match generate_from_primes(&p, &q) {
-                Ok(key_pair) => return key_pair,
-                Err(_) => continue,
-            }
-        }
-    }
-
-    pub fn write_to_file(&self) -> std::io::Result<()> {
-        self.public.write_to_file("key.public")?;
-        self.private.write_to_file("key.private")?;
-        Ok(())
-    }
-}
-
-fn generate_from_primes(p: &BigInt, q: &BigInt) -> Result<KeyPair, &'static str> {
-    let n = p * q;
-
-    let lambda_n = lcm(&(p - BigInt::from(1)), &(q - BigInt::from(1)));
-    let e = BigInt::from(DEFAULT_E);
-
-    let (_, d, _) = extended_eucledian(&e, &lambda_n);
-
-    if d < BigInt::from(0) {
-        Err("Failed to generate private key")
-    } else {
-        Ok(KeyPair {
-            public: Key {
-                exp: e,
-                modulus: n.clone(),
-            },
-            private: Key { exp: d, modulus: n },
-        })
-    }
-}
-
-fn generate_probable_prime() -> BigInt {
-    let mut num: BigInt = rand::thread_rng().sample(RandomBits::new(KEY_SIZE));
-    while !algorithms::miller_rabin(&num, MR_ITERATIONS) {
-        num = rand::thread_rng().sample(RandomBits::new(KEY_SIZE));
-    }
-    return num;
-}
-
-fn lcm(a: &BigInt, b: &BigInt) -> BigInt {
-    let (qcd, _, _) = extended_eucledian(a, b);
-    a * b / qcd
 }
